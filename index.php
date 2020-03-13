4589069c286e2e90a2e164d99d1cf18688a4d9e0
Tecnico em Informatica 2019.2 <16121742019.2@senacrjedu.local>
2020-03-13 16:43:42 -0300

    attttt

--- index.php
+++ index.php
@@ -1,156 +1,201 @@
+<?php
+//include_once("config.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/cores.css">
-    <link rel="stylesheet" href="css/estilo.css">
-    <link rel="stylesheet" href="css/fixos.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/cores.css">
+  <link rel="stylesheet" href="css/estilo.css">
+  <link rel="stylesheet" href="css/fixos.css">
 </head>
+
 <body class="bg-cinza">
-    <header class="text-center bg-light">
+  <header class="text-center bg-light">
+    <img src="img/logo.png" alt="logotipo" class="logo">
+  </header>
 
-        <img src="img/logo.png" alt="logotipo" class="logo">
+  <nav class="navbar navbar-expand-lg  navbar-light bg-light sticky-top m-0">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        
-        <ul class="nav">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Inicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item ml-auto">
-              <a class="nav-link" href="#">Entrar</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Cadastrar</a>
-            </li>
-        </ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
 
-        
-    </header>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Entrar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="cadastro.php">Cadastrar</a>
+        </li>
 
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="img/c1.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5></h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="img/c2.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="img/c3.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </div>
-          </div>
+      </ul>
+      <!-- 
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?pag=perfil"><?= $_SESSION["user"]["nome"] ?></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?pag=log&action=off">Sair</a>
+        </li>
+      </ul> -->
+
+
+    </div>
+  </nav>
+
+  <?php
+  // include_once('pages/mensagens.php');
+  if (!empty($_GET)) {
+    if ($_GET["pag"] == "cad") {
+      // include("pages/formUser.php");
+    } else if ($_GET["pag"] == "log") {
+      //include("pages/login.php");
+    } else if ($_GET["pag"] == "perfil") {
+      //include("pages/perfilUser.php");
+    } else {
+      // include("pages/inicio.php");
+    }
+  } else {
+    //include("pages/inicio.php");
+  }
+  ?>
+
+
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/c1.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5></h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+      </div>
+      <div class="carousel-item">
+        <img src="img/c2.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/c3.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <section class="container conteudo vantagens">
+
+    <div class="row">
+
+      <div class="col-md-6 col-12">
+
+        <div class="col-md-12">
+
+          <h5>lorem</h5>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptates error, atque sint aliquid quas recusandae consequuntur similique culpa amet repellendus, itaque dolores iusto voluptate, aut nulla nostrum dolorum dicta?</p>
+
+        </div>
+
+        <div class="col-md-12">
+
+          <h5>lorem</h5>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptates error, atque sint aliquid quas recusandae consequuntur similique culpa amet repellendus, itaque dolores iusto voluptate, aut nulla nostrum dolorum dicta?</p>
+
+        </div>
+
+        <div class="col-md-12">
+
+          <h5>lorem</h5>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptates error, atque sint aliquid quas recusandae consequuntur similique culpa amet repellendus, itaque dolores iusto voluptate, aut nulla nostrum dolorum dicta?</p>
+
+        </div>
+
       </div>
 
-      <section class="container conteudo vantagens">
+      <div class="col-md-6 col-12 text-center">
 
-          <div class="row">
-            
-            <div class="col-md-6 col-12">
+        <img src="img/status.png" class="status">
 
-              <div class="col-md-12">
+      </div>
 
-                <h5>lorem</h5>
+    </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptates error, atque sint aliquid quas recusandae consequuntur similique culpa amet repellendus, itaque dolores iusto voluptate, aut nulla nostrum dolorum dicta?</p>
+  </section>
 
-              </div>
+  <footer class="bg-cinza container-fluid py-5">
+    <div class="row">
+      <div class="col-md-3">
+        <ul>
+          <li><a href="#produtos">Produtos</a></li>
+          <li><a href="#clientes">Clientes</a></li>
+          <li><a href="#lojas">Lojas</a></li>
+          <li><a href="">Referências</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <ul>
+          <li><a href="">Relacionamento</a></li>
+          <li><a href="">Trabalhe Conosco</a></li>
+          <li><a href="">Quen somos</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <a href=""><img src="img\social\limpos\twitter.png" alt="" class="redesocial"></a>
+        <a href=""><img src="img\social\limpos\facebook.png" alt="" class="redesocial"></a>
+        <a href=""><img src="img\social\limpos\youtube.png" alt="" class="redesocial"></a>
+      </div>
+      <div class="col-md-3">
+        <img src="img\pagamento.jpg" alt="" class="img-fluid">
+      </div>
+    </div>
 
-              <div class="col-md-12">
+  </footer>
 
-                <h5>lorem</h5>
-                
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptates error, atque sint aliquid quas recusandae consequuntur similique culpa amet repellendus, itaque dolores iusto voluptate, aut nulla nostrum dolorum dicta?</p>
-
-              </div>
-
-              <div class="col-md-12">
-
-                <h5>lorem</h5>
-                
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptates error, atque sint aliquid quas recusandae consequuntur similique culpa amet repellendus, itaque dolores iusto voluptate, aut nulla nostrum dolorum dicta?</p>
-
-              </div>
-
-            </div>
-
-            <div class="col-md-6 col-12 text-center">
-
-              <img src="img/status.png" class="status">
-              
-            </div>
-
-          </div>
-
-      </section>
-
-      <footer class="bg-cinza container-fluid py-5">
-        <div class="row">
-            <div class="col-md-3">
-                <ul>
-                    <li><a href="#produtos">Produtos</a></li>
-                    <li><a href="#clientes">Clientes</a></li>
-                    <li><a href="#lojas">Lojas</a></li>
-                    <li><a href="">Referências</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3">
-                <ul>
-                    <li><a href="">Relacionamento</a></li>
-                    <li><a href="">Trabalhe Conosco</a></li>
-                    <li><a href="">Quen somos</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3">
-                <a href=""><img src="img\social\limpos\twitter.png" alt="" class="redesocial"></a>
-                <a href=""><img src="img\social\limpos\facebook.png" alt="" class="redesocial"></a>
-                <a href=""><img src="img\social\limpos\youtube.png" alt="" class="redesocial"></a>
-            </div>
-            <div class="col-md-3">
-                <img src="img\pagamento.jpg" alt="" class="img-fluid">
-            </div>
-        </div>
-
-    </footer>
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="js/cep.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="js/cep.js"></script>
 </body>
+
 </html>
